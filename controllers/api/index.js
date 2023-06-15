e9c579a106346178af7f55df73bf9c81c1ee6ceb
Nate Trask <trask.nate@gmail.com>
2023-06-14 19:53:39 -0500

    added code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const workoutRoutes = require('./workoutRoutes');
+const nutritionRoutes = require('./nutritionRoutes')
 
 router.use('/workouts', workoutRoutes);
+router.use('/nutriton', nutritionRoutes);
 
 module.exports = router;
