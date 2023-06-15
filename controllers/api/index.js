97f20623502b7f821dd97e0195c6f17ce25a1c38
Bekam <mspbekan@gmail.com>
2023-06-15 10:16:40 -0500

    Committing local changes before merge

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const workoutRoutes = require('./workoutRoutes');
 const nutritionRoutes = require('./nutritionRoutes')
+// const caloriesRoutes = require('./caloriesRoutes')
 
 router.use('/workouts', workoutRoutes);
 router.use('/nutriton', nutritionRoutes);
+// router.use('/calories', caloriesRoutes);
 
 module.exports = router;
