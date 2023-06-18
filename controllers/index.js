ae08919b32d1e6c5e9bcdf07ce8cc2562bd8f07e
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-06-18 13:54:15 -0500

    login and signup working. Also started working on Bulma

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const signupRoutes = require('./signupRoutes');
 const loginRoute = require('./loginRoute');
-
+const logoutRoute = require('./logoutRoute');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+
 router.use('/signup', signupRoutes);
 router.use('/login', loginRoute);
-
+router.use('/logout', logoutRoute);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
