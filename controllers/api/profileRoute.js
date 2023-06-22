6114b91b748fd045646954529151f6be36f92462
Nate Trask <trask.nate@gmail.com>
2023-06-22 15:31:33 -0500

    removed unneeded files

--- controllers/api/profileRoute.js
+++ controllers/api/profileRoute.js
@@ -1,21 +1,21 @@
-const withAuth = require('../utils/auth');
-const {Workout} = require('../../models');
+// const withAuth = require('../utils/auth');
+// const {Workout} = require('../../models');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    // Retrieve saved workouts for the logged-in user
-    const savedWorkouts = await Workout.findAll({
-      where: { user_id: req.session.user_id },
-    });
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     // Retrieve saved workouts for the logged-in user
+//     const savedWorkouts = await Workout.findAll({
+//       where: { user_id: req.session.user_id },
+//     });
 
-    res.render('profile', {
-      logged_in: true,
-      workouts: savedWorkouts,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while retrieving saved workouts' });
-  }
-});
+//     res.render('profile', {
+//       logged_in: true,
+//       workouts: savedWorkouts,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'An error occurred while retrieving saved workouts' });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
